ac9e847c2d55d127e108e840d6b93e30d19268c7
Erce Altun <ercealtun0655@gmail.com>
2022-05-21 22:27:13 +0300

    UserProfile updated

--- CANBOOKRAM/Models/UserProfile.cs
+++ CANBOOKRAM/Models/UserProfile.cs
@@ -11,5 +11,10 @@ namespace CANBOOKRAM.Models
         public int Id { get; set; }
         public string? UserId { get; set; }
         public byte[]? ProfilePicture { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? City { get; set; }
+        public DateTime? BirthDate { get; set; }
+
     }
 }
