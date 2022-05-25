750277c0bb3306e9ea7f5bd3aac2e80294dfd321
Erce Altun <ercealtun0655@gmail.com>
2022-05-25 23:34:26 +0300

    Rating added bismillah.

--- CANBOOKRAM/Models/Friends.cs
+++ CANBOOKRAM/Models/Friends.cs
@@ -5,7 +5,7 @@ namespace CANBOOKRAM.Models
     public class Friends
     {
         [Key]
-        //public int Id { get; set; }
+        public int Id { get; set; }
         public string? UserId { get; set; }
         public string? FriendId { get; set; }
         public bool Approved { get; set; }
